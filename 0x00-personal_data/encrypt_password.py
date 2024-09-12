1b65a908a39dc2dbf0eda8a60275d1fd02da6595
Merch7x <aquamaingi@gmail.com>
2024-09-12 23:02:16 +0300

    Password Hashing

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Implement password hashing"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hash a password"""
+    bp = password.encode('utf-8')
+    hashed = bcrypt.hashpw(bp, bcrypt.gensalt())
+    return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check the validity of a password"""
+    byte_pas = password.encode('utf-8')
+    if bcrypt.checkpw(byte_pas, hashed_password):
+        return True
