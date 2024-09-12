6013c7e0a053b204c895bbbbbe949e46f15861f0
Merch7x <aquamaingi@gmail.com>
2024-09-12 22:04:06 +0300

    Update type annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -51,7 +51,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """Returns a connector to the database"""
     connection = mysql.connector.connect(
         host="localhost" or os.environ.get('PERSONAL_DATA_DB_HOST'),
