100bc81559d030e976bc96f329c034817a6c7ee2
Merch7x <aquamaingi@gmail.com>
2024-09-12 17:39:46 +0300

    Use loggin formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """filters personal information in logs"""
 import re
+import logging
 
 
 def filter_datum(fields, redaction, message, separator):
@@ -10,3 +11,24 @@ def filter_datum(fields, redaction, message, separator):
     pattern = '|'.join([f'{field}=[^{separator}]*' for field in fields])
     return re.sub(pattern, lambda match:
                   f'{match.group(0).split("=")[0]}={redaction}', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: list[str]):
+        """Object initialiser"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Formats and reducts the recorrd accordingly"""
+        message = super().format(record)
+        r_message = filter_datum(self.fields, self.REDACTION,
+                                 message, self.SEPARATOR)
+        return r_message
