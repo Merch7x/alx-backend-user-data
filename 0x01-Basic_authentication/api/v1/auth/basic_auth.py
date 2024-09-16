4a488aa4df234c2286de1472e9532d2db85a9e3d
Merch7x <aquamaingi@gmail.com>
2024-09-16 20:19:17 +0300

    Decode base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ with an encode byte string of
   a password and User
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -22,3 +23,17 @@ class BasicAuth(Auth):
             return None
 
         return h_list[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """returns the decode value of a Base64 header"""
+        if base64_authorization_header is None or\
+                not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            value = base64.b64decode(
+                base64_authorization_header)
+            return value.decode('utf-8')
+        except Exception:
+            return None
