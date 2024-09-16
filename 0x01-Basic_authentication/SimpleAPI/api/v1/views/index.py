34a4c8b357c95b884a1976b47e351901e37cf2f2
Merch7x <aquamaingi@gmail.com>
2024-09-16 15:22:41 +0300

    Create forbidden error handler

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -33,3 +33,13 @@ def unauthorized() -> str:
       - the number of each objects
     """
     return jsonify(abort(401))
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - an error when access to a resource is
+      forbidden
+    """
+    return jsonify(abort(403))
