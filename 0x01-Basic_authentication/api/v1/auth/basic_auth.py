7088c1dcdca5dbf54b3f952987e7bae0cdddd4d3
Merch7x <aquamaingi@gmail.com>
2024-09-16 19:28:00 +0300

    Fix linter errors

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Handle authentication to a server
-  with an encode byte string of 
+with an encode byte string of
   a password and User
 """
 from api.v1.auth.auth import Auth
