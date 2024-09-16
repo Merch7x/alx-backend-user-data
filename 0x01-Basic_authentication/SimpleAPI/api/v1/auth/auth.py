f4c03c6f0f29b82a408e5819d3d8358e741577f3
Merch7x <aquamaingi@gmail.com>
2024-09-16 16:07:48 +0300

    Implement require_auth

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -9,6 +9,18 @@ class Auth:
     def require_auth(self, path: str, excluded_paths:
                      List[str]) -> bool:
         """Define paths that require authentication"""
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        n_path = path.rstrip('/')
+
+        n_e_p = [p.rstrip('/') for p in excluded_paths]
+        if n_path not in n_e_p:
+            return True
+
         return False
 
     def authorization_header(self, request=None) -> str:
