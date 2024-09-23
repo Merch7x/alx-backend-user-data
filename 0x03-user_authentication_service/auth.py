550f7d7f4ce9a1ab16451f8f2cd0b12f6379d973
Merch7x <aquamaingi@gmail.com>
2024-09-23 14:54:31 +0300

    Create session ids through uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,12 +4,18 @@ from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
     """Create a hashed_password"""
     p_hash = hashpw(password.encode('utf-8'), gensalt())
     return p_hash
+
+
+def _generate_uuid() -> str:
+    """generate uuid"""
+    return str(uuid4())
 
 
 class Auth:
@@ -43,3 +49,15 @@ class Auth:
                     return False
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Creates a session id"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                session_id = _generate_uuid()
+                user_id = int(user.id)
+                self._db.update_user(user_id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return
