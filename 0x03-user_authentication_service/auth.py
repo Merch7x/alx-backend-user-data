fd2f7d08c8b6ccd7db403b9f5c92e01b8800f6c4
Merch7x <aquamaingi@gmail.com>
2024-09-23 15:53:48 +0300

    Remove a users session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -61,3 +61,21 @@ class Auth:
             return session_id
         except NoResultFound:
             return
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Finds a user by session ID"""
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            if user:
+                return user
+        except NoResultFound:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroys a users session"""
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except ValueError:
+            return
