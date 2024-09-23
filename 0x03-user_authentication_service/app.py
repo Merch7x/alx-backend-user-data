874d5b59cdc5947041ff258ce6a700783b3ea997
Merch7x <aquamaingi@gmail.com>
2024-09-23 12:01:47 +0300

    Create a flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A flask application"""
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Index route"""
+    return jsonify({"message": "Bienvenue"}), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
