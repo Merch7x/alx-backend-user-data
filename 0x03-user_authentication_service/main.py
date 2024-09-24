ed7adcbf89605df40f5a91b234cc0cd8b28e232f
Merch7x <aquamaingi@gmail.com>
2024-09-24 22:27:28 +0300

    Add Integration tests

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
     log_in_wrong_password(EMAIL, NEW_PASSWD)
     profile_unlogged()
     session_id = log_in(EMAIL, PASSWD)
-    # profile_logged(session_id)
+    profile_logged(session_id)
     log_out(session_id)
     reset_token = reset_password_token(EMAIL)
     update_password(EMAIL, reset_token, NEW_PASSWD)
