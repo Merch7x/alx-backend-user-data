8faa0cd9659ee9ea7a78f8e4c4a631f4f8605a04
Merch7x <aquamaingi@gmail.com>
2024-09-24 22:35:33 +0300

    Update users route status code

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -21,13 +21,13 @@ def users():
     email = request.form.get("email")
     password = request.form.get("password")
 
-    if not email or not password:
-        return jsonify({"message": "email already registered"}), 400
+    # if not email or not password:
+    #     return jsonify({"message": "email already registered"}), 400
 
     try:
         AUTH.register_user(email, password)
         return jsonify(
-            {"email": f"{email}", "message": "user created"}), 201
+            {"email": f"{email}", "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
