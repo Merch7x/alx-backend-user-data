e0dccca180359514616130fff966ae25de78436e
Merch7x <aquamaingi@gmail.com>
2024-09-24 17:50:58 +0300

    Update token generation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,7 +83,7 @@ class Auth:
         """Generates a reset passowrd token"""
         try:
             user = self._db.find_user_by(email=email)
-            token = uuid4()
+            token = _generate_uuid()
             self._db.update_user(user.id, reset_token=token)
             return token
         except NoResultFound:
